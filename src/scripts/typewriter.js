6328b5412f8b7bceff775b94618c9b1c3c5a2bf7
BeatrizAguillera <beatriz.aguillera@usp.br>
2022-08-26 02:33:55 -0300

    typeWriter algorithm updated
    First algorithm update and bug fixed;
    Some small text bugs review

--- src/scripts/typewriter.js
+++ src/scripts/typewriter.js
@@ -1,8 +1,8 @@
 /* Funcionamento do typeWriter em passos:
     1. A function typeWriter transforma o innerText do elemento escolhido em uma array e o .split divide o texto colocando cada caractere como uma posição diferente da array (incluindo espaços);
     2. Inicialmente, apagamos o texto original do elemento para que o efeito de escrita possa acontecer;
-    3. A função pega cada caractere dentro da array e marca um tempo específico para ela aparecer na tela usando o setTimeout. Cada caractere aparece em um tempo determinado pelo index multiplicado. Isso gera o efeito de uma letrinha aparecendo após a outra;
-    5. Para que o efeito se repita infinitamente, usamos o setInterval para repetir a função typeWriter dentro de um determinado tempo.
+    3. A função pega cada caracter dentro da array e marca um tempo específico para ela aparecer na tela usando o setTimeout. Cada caracter aparece em um tempo determinado pelo index multiplicado. Isso gera o efeito de uma letrinha aparecendo após a outra;
+    5. Para que o efeito se repita infinitamente, usamos uma função que simula o setInterval, mas antes aplica um callback por precaução, para que as letras não se embaralhem na tela em detetrimento de um bug do setInterval (que não respeita o ciclo de funcionamento da função antes de repeti-la)
     */
 
 function typeWriter(element, timing) {
@@ -14,12 +14,26 @@ function typeWriter(element, timing) {
 }
 
 const title = document.querySelector('.egTitle');
-typeWriter(title, 200);
-window.setInterval(() => typeWriter(title, 200), 15000);
-
 const subtitle = document.querySelector('#mainSubtitle');
-typeWriter(subtitle, 90);
-window.setInterval(() => typeWriter(subtitle, 90), 10000);
 
+function manualSetInterval(callback, duration, callbackArguments) {
+    callback.apply(this, callbackArguments);
+    var args = arguments,
+        scope = this;
 
+    setTimeout(function () {
+        manualSetInterval.apply(scope, args);
+    }, duration);
+}
 
+function typeWriterTitle() {
+    typeWriter(title, 200);
+}
+
+function typeWriterSubtitle() {
+    typeWriter(subtitle, 90);
+}
+
+manualSetInterval(typeWriterTitle, 15000);
+
+manualSetInterval(typeWriterSubtitle, 13000); 
